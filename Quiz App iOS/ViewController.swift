409e0f10def943335721357190f2feaf4989c03d
ash-old <ashley.oldham@gmail.com>
2021-03-22 21:34:35 +0000

    UI tweaks and button click moves answer

--- Quiz App iOS/ViewController.swift
+++ Quiz App iOS/ViewController.swift
@@ -15,9 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let quiz = [
-        "Four + Two is equal to Six",
-        "Five - Three is greater than One",
-        "Three + Eight is less than Ten"
+        ["Four + Two is equal to Six", "True"],
+        ["Five - Three is greater than One", "True"],
+        ["Three + Eight is less than Ten", "False"]
         ]
     
     var questionNumber = 0
@@ -29,12 +29,23 @@ class ViewController: UIViewController {
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        questionNumber += 1
-        updateUI()
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Correct!")
+        } else {
+            print("Wrong!")
+        }
+        
+            if questionNumber + 1 < quiz.count {
+                questionNumber += 1
+                updateUI()
+            }
     }
     
     func updateUI() {
-        questionLabel.text = quiz[questionNumber]
+        questionLabel.text = quiz[questionNumber][0]
     }
     
 }
