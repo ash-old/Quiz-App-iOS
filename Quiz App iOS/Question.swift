16cb1194b981c8d909af489d67d091fb5189d86b
ash-old <ashley.oldham@gmail.com>
2021-03-28 23:31:53 +0100

    learning about the struct feature so implemented this in my question array.

---
+++ Quiz App iOS/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  Quiz App iOS
+//
+//  Created by Ash Oldham on 28/03/2021.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
